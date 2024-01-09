b14974452fc681dc0576663a3ff6ede8a9170fdc
Wiktor <wiktor.toczylowski2@gmail.com>
2024-01-09 17:58:06 +0100

    Added Mapper and mappingprofile, registered it

---
+++ ProjectManager.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Application.Mapping;
+using ProjectManager.Application.Project.Commands.CreateProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateProjectCommand));
+
+            services.AddAutoMapper(typeof(ProjectMappingProfile));
+
+            services.AddValidatorsFromAssemblyContaining<CreateProjectCommandValidator>()
+                   .AddFluentValidationAutoValidation()
+                   .AddFluentValidationClientsideAdapters();
+        }
+    }
+}
---
+++ ProjectManager.Application/Mapping/ProjectMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProjectManager.Application.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Mapping
+{
+    public class ProjectMappingProfile : Profile
+    {
+        public ProjectMappingProfile()
+        {
+            CreateMap<Domain.Entities.Project, ProjectDto>()
+                .ReverseMap();
+        }
+    }
+}
