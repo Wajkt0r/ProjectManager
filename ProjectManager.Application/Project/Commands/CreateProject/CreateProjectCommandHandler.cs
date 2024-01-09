9339f810b77919b051afff30dc3a42f16c6513af
Wiktor <wiktor.toczylowski2@gmail.com>
2024-01-09 17:55:53 +0100

    Added CQRS to the project, set up CreateCommand

---
+++ ProjectManager.Application/Project/Commands/CreateProject/CreateProjectCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Commands.CreateProject
+{
+    public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand>
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
+
+        public CreateProjectCommandHandler(IProjectRepository projectRepository, IMapper mapper)
+        {
+            _projectRepository = projectRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = _mapper.Map<Domain.Entities.Project>(request);
+            project.EncodeName();
+
+            await _projectRepository.Create(project);
+            return Unit.Value;
+        }
+    }
+}
---
+++ ProjectManager.Application/Project/Commands/CreateProject/CreateProjectCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Commands.CreateProject
+{
+    public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+    {
+        public CreateProjectCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .MinimumLength(5).WithMessage("Nazwa projektu musi miec minimum 5 znakow")
+                .MaximumLength(50).WithMessage("Nazwa projektu nie moze przekraczac 50 znakow");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .MinimumLength(10).WithMessage("Prosze uzupelnic opis projektu, musi on zawierac minimum 10 znakow");
+
+            RuleFor(p => p.FinishDate)
+                .NotEmpty().WithMessage("Ustaw date koncowa projektu, mozesz ja przesunac pozniej")
+                .GreaterThan(DateTime.UtcNow);
+        }
+    }
+}
