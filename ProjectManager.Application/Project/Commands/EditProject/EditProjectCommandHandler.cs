1acea037f105abda6b66e6a1ee290b49132f9330
Wiktor <wiktor.toczylowski2@gmail.com>
2024-01-12 18:10:35 +0100

    Created command and handler for edit project action

---
+++ ProjectManager.Application/Project/Commands/EditProject/EditProjectCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Commands.EditProject
+{
+    public class EditProjectCommandHandler : IRequestHandler<EditProjectCommand>
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public EditProjectCommandHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+        public async Task<Unit> Handle(EditProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = _projectRepository.GetByEncodedName(request.EncodedName);
+
+            project.Description = request.Description;
+            project.FinishDate = request.FinishDate;
+
+            await _projectRepository.Commit();
+            return Unit.Value;
+            
+        }
+    }
+}
