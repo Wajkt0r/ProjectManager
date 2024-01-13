f61a989357d906a3d40033c9f51c093fca4259f5
Wiktor <wiktor.toczylowski2@gmail.com>
2024-01-13 21:14:02 +0100

    Created query to get project by encoded name, little cshtml changes

---
+++ ProjectManager.Application/Project/Queries/GetProjectByEncodedName/GetProjectByEncodedNameQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Queries.GetProjectByEncodedName
+{
+    public class GetProjectByEncodedNameQuery : IRequest<ProjectDto>
+    {
+        public string EncodedName { get; set; }
+
+        public GetProjectByEncodedNameQuery(string encodedName)
+        {
+            EncodedName = encodedName;
+        }
+    }
+}
---
+++ ProjectManager.Application/Project/Queries/GetProjectByEncodedName/GetProjectByEncodedNameQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Project.Queries.GetProjectByEncodedName
+{
+    public class GetProjectByEncodedNameQueryHandler : IRequestHandler<GetProjectByEncodedNameQuery, ProjectDto>
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
+
+        public GetProjectByEncodedNameQueryHandler(IProjectRepository projectRepository, IMapper mapper)
+        {
+            _projectRepository = projectRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProjectDto> Handle(GetProjectByEncodedNameQuery request, CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository.GetByEncodedName(request.EncodedName);
+            var projectDto = _mapper.Map<ProjectDto>(project);
+
+            return projectDto;
+        }
+    }
+}
