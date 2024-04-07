09098f3ff130a76b054b4e9dc3c1dcf91f487e84
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-04-07 13:21:13 +0200

    Added relation from ProjectTask to Project

--- ProjectManager.Infrastructure/Persistence/ProjectManagerDbContext.cs
+++ ProjectManager.Infrastructure/Persistence/ProjectManagerDbContext.cs
@@ -12,12 +12,19 @@ namespace ProjectManager.Infrastructure.Persistence
     public class ProjectManagerDbContext : IdentityDbContext
     {
         public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> Tasks { get; set; }
+       
 
         public ProjectManagerDbContext(DbContextOptions<ProjectManagerDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(p => p.ProjectTasks)
+                .WithOne(t => t.Project)
+                .HasForeignKey(t => t.ProjectId);
         }
 
     }
