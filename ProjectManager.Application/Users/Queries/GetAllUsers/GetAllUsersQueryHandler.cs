83e09d0f18a16d085715eeaaedaf9c379533ab91
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-10 23:28:53 +0200

    Added Query to get all users

---
+++ ProjectManager.Application/Users/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Queries.GetAllUsers
+{
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserDto>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public GetAllUsersQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<UserDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllUser();
+
+            if (users == null)
+            {
+                return null;
+            }
+
+            var usersDto = _mapper.Map<IEnumerable<UserDto>>(users);
+
+            return usersDto;
+        }
+    }
+}
