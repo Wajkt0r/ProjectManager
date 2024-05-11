dd2a62ee6f5146b3a3b327bc5d8447efeb0ba62a
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-11 14:23:35 +0200

    Created UserController and action to get all users

---
+++ ProjectManager.MVC/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Application.Users.Queries.GetAllUsers;
+
+namespace ProjectManager.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public UserController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        [Route("AdminPanel/Users")]
+        public async Task<IActionResult> Users()
+        {
+            var users = await _mediator.Send(new GetAllUsersQuery());
+            return View("Users", users);
+        }
+    }
+}
