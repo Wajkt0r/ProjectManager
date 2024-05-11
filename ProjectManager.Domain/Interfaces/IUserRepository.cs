2851776e09a73802dc6cbd6805b2f8e5d0449936
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-11 14:24:13 +0200

    Created UserRepository and added action for getting all users

---
+++ ProjectManager.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProjectManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Commit();
+        Task<IEnumerable<User>>GetAllUsers();
+    }
+}
---
+++ ProjectManager.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Domain.Entities;
+using ProjectManager.Domain.Interfaces;
+using ProjectManager.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ProjectManagerDbContext _dbContext;
+        public UserRepository(ProjectManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+            => await _dbContext.Users.ToListAsync();
+    }
+}
