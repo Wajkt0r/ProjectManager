4be4220d7412bab09b5ebc44e4dcf3423bc4e8f5
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-16 23:50:32 +0200

    Created Query for getting user by email

---
+++ ProjectManager.Application/Users/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Queries.GetUserByEmail.GetUserByEmail
+{
+    public class GetUserByEmailQuery : IRequest<UserDto>
+    {
+        public string Email { get; set; } = default!;
+    }
+}
---
+++ ProjectManager.Application/Users/Queries/GetUserByEmail/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using ProjectManager.Application.Users.Queries.GetUserByEmail.GetUserByEmail;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Queries.GetUserByEmail
+{
+    public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, UserDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public GetUserByEmailQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByEmail(request.Email);
+
+            var userDto = _mapper.Map<UserDto>(user);
+
+            return userDto;
+        }
+    }
+}
