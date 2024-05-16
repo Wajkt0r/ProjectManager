7975b2daf83102c9d61f7b198d5f435b7734d86e
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-16 23:50:21 +0200

    Changed Users view name to Index

--- ProjectManager.MVC/Controllers/UserController.cs
+++ ProjectManager.MVC/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Application.Users;
 using ProjectManager.Application.Users.Queries.GetAllUsers;
+using ProjectManager.Domain.Entities;
 
 namespace ProjectManager.MVC.Controllers
 {
@@ -9,18 +12,21 @@ namespace ProjectManager.MVC.Controllers
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
 
-        public UserController(IMediator mediator, IMapper mapper)
+        public UserController(IMediator mediator, IMapper mapper, UserManager<User> userManager)
         {
             _mediator = mediator;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
-        [Route("AdminPanel/Users")]
-        public async Task<IActionResult> Users()
+        [Route("AdminPanel/Index")]
+        public async Task<IActionResult> Index()
         {
             var users = await _mediator.Send(new GetAllUsersQuery());
-            return View("Users", users);
+
+            return View("Index", users);
         }
     }
 }
