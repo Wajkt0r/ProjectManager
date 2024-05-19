71d1ce8382df64ebb2918978f877306ea719ac0f
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-20 01:15:14 +0200

    Changed partial views for the previous user actions and added js implementation for delete user action

--- ProjectManager.MVC/wwwroot/js/User/UserManagment.js
+++ ProjectManager.MVC/wwwroot/js/User/UserManagment.js
@@ -1,3 +1,29 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const deleteModal = document.getElementById('deleteUserModal');
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+        const button = event.relatedTarget;
+        const userEmail = button.getAttribute('data-username')
 
-}
+        const confirmButton = deleteModal.querySelector('#confirmDelete');
+
+        const deleteUserFunction = () => {
+            $.ajax({
+                url: `/AdminPanel/User/${userEmail}/Delete`,
+                type: 'POST',
+                success: function (response) {
+                    toastr["success"]("Deleted user");
+                },
+                error: function () {
+                    toastr["error"]("Something went wrong");
+                }
+            });
+        };
+
+        confirmButton.removeEventListener('click', deleteUserFunction);
+        confirmButton.addEventListener('click', deleteUserFunction);
+
+        const usernameSpan = deleteModal.querySelector('#delete-modal-username');
+        usernameSpan.textContent = userEmail;
+    });
+
+});
