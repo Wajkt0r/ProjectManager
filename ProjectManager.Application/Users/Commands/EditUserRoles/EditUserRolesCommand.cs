ad1995e67c5482e3763789d9a94c77454b4d62fa
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-05-19 22:42:35 +0200

    Code refactor

---
+++ ProjectManager.Application/Users/Commands/EditUserRoles/EditUserRolesCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Commands.EditUserRoles
+{
+    public class EditUserRolesCommand : IRequest
+    {
+        public string Email { get; set; } = default!;
+        public List<string> Roles { get; set; }
+    }
+}
---
+++ ProjectManager.Application/Users/Commands/EditUserRoles/EditUserRolesCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProjectManager.Application.ApplicationUser;
+using ProjectManager.Domain.Entities;
+using ProjectManager.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Users.Commands.EditUserRoles
+{
+    public class EditUserRolesCommandHandler : IRequestHandler<EditUserRolesCommand>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserManager<User> _userManager;
+        public EditUserRolesCommandHandler(IUserRepository userRepository, UserManager<User> userManager)
+        {
+            _userRepository = userRepository;
+            _userManager = userManager;
+        }
+        public async Task<Unit> Handle(EditUserRolesCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserByEmail(request.Email);
+
+            if (user == null)
+            {
+                return Unit.Value;  
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            foreach (var role in request.Roles)
+            {
+                if (role in userRoles) {
+                    // Jak jest to skip fora
+                }
+
+                // powyzszy if sie nie wykona wiec dodajemy role
+                var result = await _userManager.AddToRoleAsync(user, role); // cos z tym mozna pokmninic
+            }
+
+            
+        }
+    }
+}
