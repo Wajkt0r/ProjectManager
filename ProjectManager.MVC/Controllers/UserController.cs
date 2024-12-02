1fb02d743329af112fb7e2349c8a1fb7e1c05594
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-12-02 13:29:45 +0100

    Added authorization for Admin page

--- ProjectManager.MVC/Controllers/UserController.cs
+++ ProjectManager.MVC/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -27,6 +28,7 @@ namespace ProjectManager.MVC.Controllers
             _userManager = userManager;
         }
 
+        [Authorize(Roles="Admin")]
         [Route("AdminPanel/Index")]
         public async Task<IActionResult> Index()
         {
