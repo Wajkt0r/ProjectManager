384c8a7b66304ff9045e0c70389bd2ea49c95abc
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-12-08 02:32:24 +0100

    Add Query GetAllProjectContributors and handler

---
+++ ProjectManager.Application/Project/Queries/GetAllProjectContributors/GetAllProjectContributorsQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using ProjectManager.Application.Users;
+
+namespace ProjectManager.Application.Project.Queries.GetAllProjectContributors
+{
+    public class GetAllProjectContributorsQuery : IRequest<IEnumerable<UserDto>>
+    {
+        public string ProjectEncodedName { get; set; } = default!;
+
+        public GetAllProjectContributorsQuery(string encodedName)
+        {
+            ProjectEncodedName = encodedName;
+        }
+    }
+}
---
+++ ProjectManager.Application/Project/Queries/GetAllProjectContributors/GetAllProjectContributorsQueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProjectManager.Application.Users;
+using ProjectManager.Domain.Entities;
+using ProjectManager.Domain.Interfaces;
+
+namespace ProjectManager.Application.Project.Queries.GetAllProjectContributors
+{
+    public class GetAllProjectContributorsQueryHandler : IRequestHandler<GetAllProjectContributorsQuery, IEnumerable<UserDto>>
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        public GetAllProjectContributorsQueryHandler(IProjectRepository projectRepository, IMapper mapper, UserManager<User> userManager)
+        {
+            _projectRepository = projectRepository;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+        public async Task<IEnumerable<UserDto>> Handle(GetAllProjectContributorsQuery request, CancellationToken cancellationToken)
+        {
+            var projectId = await _projectRepository.GetProjectId(request.ProjectEncodedName);
+
+            var projectContributors = await _projectRepository.GetProjectContributors(projectId);
+
+            var projectContributorsDto = new List<UserDto>();
+
+            foreach (var contributor in projectContributors)
+            {
+                var user = _userManager.Users.Where(u => u.Id == contributor.UserId).FirstOrDefault();
+                if (user != null)
+                {
+                    var userRoles = await _userManager.GetRolesAsync(user);
+                    var contributorDto = new UserDto
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        Roles = (List<string>)userRoles
+                    };
+                    projectContributorsDto.Add(contributorDto);                   
+                }
+            }
+
+            return projectContributorsDto;
+
+        }
+    }
+}
