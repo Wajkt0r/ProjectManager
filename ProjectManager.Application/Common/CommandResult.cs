c28234e2691b5985d3eb73b74dcfce4c3ee571f3
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-12-16 16:41:59 +0100

    Add CommandResult dto for returning status codes and messages from CQRS

---
+++ ProjectManager.Application/Common/CommandResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Common
+{
+    public class CommandResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public static CommandResult Success(string Message = "Success")
+            => new CommandResult { IsSuccess = true, Message = Message, StatusCode = 200 };
+
+        public static CommandResult Failure(string message, int statusCode = 400)
+            => new CommandResult { IsSuccess = false, Message = message, StatusCode = statusCode };
+    }
+}
