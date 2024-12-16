607b08df44dadd321e71fefe24be6b943f75ae40
Wajkt0r <wiktor.toczylowski2@gmail.com>
2024-12-16 13:41:51 +0100

    Add GetProjectIdByEncodedName Query

---
+++ ProjectManager.Application/Project/Queries/GetProjectIdByEncodedName/GetProjectIdByEncodedNameQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace ProjectManager.Application.Project.Queries.GetProjectIdByEncodedName
+{
+    public class GetProjectIdByEncodedNameQuery : IRequest<int>
+    {
+        public string ProjectEncodedName { get; set; } = default;
+    }
+}
---
+++ ProjectManager.Application/Project/Queries/GetProjectIdByEncodedName/GetProjectIdByEncodedNameQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using ProjectManager.Domain.Interfaces;
+
+namespace ProjectManager.Application.Project.Queries.GetProjectIdByEncodedName
+{
+    public class GetProjectIdByEncodedNameQueryHandler : IRequestHandler<GetProjectIdByEncodedNameQuery, int>
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public GetProjectIdByEncodedNameQueryHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+        public async Task<int> Handle(GetProjectIdByEncodedNameQuery request, CancellationToken cancellationToken)
+        {
+            var projectId = await _projectRepository.GetProjectId(request.ProjectEncodedName);
+
+            return projectId;
+        }
+    }
+}
