7ddc9172b1c2e30434da880805f24a1f917fce77
Wiktor <wiktor.toczylowski2@gmail.com>
2024-12-25 23:07:35 +0100

    Add Error Controller, model and view for returning custom errors

---
+++ ProjectManager.Application/Common/ErrorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Common
+{
+    public class ErrorViewModel
+    {
+        public string Title { get; set; } = default!;
+        public string Message { get; set; } = default!;
+        public string ActionName { get; set; } = default!;
+        public string ControllerName { get; set; } = default!; 
+    }
+}
---
+++ ProjectManager.MVC/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Application.Common;
+
+namespace ProjectManager.MVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/HandleError")]
+        public ActionResult HandleError(string title, string message, string actionName, string controllerName)
+        {
+            var model = new ErrorViewModel
+            {
+                Title = title,
+                Message = message,
+                ActionName = actionName,
+                ControllerName = controllerName
+            };
+
+            return View("Error", model);
+        }
+    }
+}
