32eac5291d4d99462c09a63606b03236d7907096
Wajkt0r <wiktor.toczylowski2@gmail.com>
2025-01-06 21:08:52 +0100

    Add UserName to register form

--- ProjectManager.MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProjectManager.MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,6 +74,10 @@ namespace ProjectManager.MVC.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(20, MinimumLength = 5)]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -118,7 +122,7 @@ namespace ProjectManager.MVC.Areas.Identity.Pages.Account
             {
                 var user = (User)CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
