3a769cfc2ca40bb94cf34cc9820e5391bb93bb66
Wajkt0r <wiktor.toczylowski2@gmail.com>
2025-01-10 18:47:13 +0100

    Add TimeLog entity and set up db for it

---
+++ ProjectManager.Domain/Entities/TimeLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Entities
+{
+    public class TimeLog
+    {
+        public int Id { get; set; }
+        public string CommitMessage { get; set; } = default!;
+        public TimeSpan TimeSpent { get; set; } = default!;
+        public ProjectTask LoggedInTask { get; set; } = default!;
+        public int LoggedInTaskId { get; set; }
+        public User LoggedBy { get; set; } = default!;
+        public string LoggedById { get; set; } = default!;
+        public DateTime LoggedAt { get; set; } = DateTime.Now;
+    }
+}
